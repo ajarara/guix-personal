ad203e0ec9196eaf1485f5460f506159163c2626
Ahmad Jarara <git@ajarara.io>
2022-03-01 20:17:22 -0500

    Add libssh-with-sk-support

---
+++ personal/guix-with-sk-support.scm
@@ -0,0 +1,20 @@
+(use-modules (gnu)
+             (guix transformations)
+             (guix packages))
+
+(letrec ((libssh-as-git-transformer
+          (options->transformation
+           '((with-git-url . "libssh=https://git.libssh.org/projects/libssh.git"))))
+         (libssh-with-commit-transformer
+          (options->transformation
+           '((with-commit . "libssh=04ae110c612f0dabc03882a52bf5be56be560020"))))
+         (libssh-with-patch-transformer
+          (options->transformation
+           '((with-patch . "libssh=./patches/0001-Add-sk-ssh-ed25519-to-PUBLIC_KEY_ALGORITHMS.patch"))))
+         (libssh-with-sk-support-transformer
+          (compose
+           libssh-with-patch-transformer
+           libssh-with-commit-transformer
+           libssh-as-git-transformer)))
+  (libssh-with-sk-support-transformer
+   (specification->package "guix")))
